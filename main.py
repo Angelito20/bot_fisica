3c2676037d1976985587932f911fea810d5aeab9
Angelito20 <71731638+Angelito20@users.noreply.github.com>
2026-02-07 13:12:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
 from fastapi import FastAPI, Request
-import requests
-import os
 
 app = FastAPI()
 
-TOKEN = os.getenv("8547596816:AAEBVBLGXXNowDnx7TBY2ZZjV2U9K44Iye0")
-API_URL = f"https://api.telegram.org/bot{TOKEN}"
-
 @app.get("/")
-async def root():
-    return {"status": "OK"}
+def root():
+    return {"status": "Bot activo ✅"}
 
 @app.post("/webhook")
-async def webhook(request: Request):
+async def telegram_webhook(request: Request):
     data = await request.json()
-    print("UPDATE:", data)
-
-    if "message" not in data:
-        return {"ok": True}
-
-    chat_id = data["message"]["chat"]["id"]
-    text = data["message"].get("text", "mensaje sin texto")
-
-    r = requests.post(
-        f"{API_URL}/sendMessage",
-        json={
-            "chat_id": chat_id,
-            "text": f"✅ Te leí: {text}"
-        }
-    )
-
-    print("RESPUESTA TELEGRAM:", r.text)
+    print(data)
     return {"ok": True}
-
